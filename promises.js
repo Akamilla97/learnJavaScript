36de0c7e42b4c6b5986e7efa573e6637bc059207
Akamilla97 <abdullkamilla97@gmail.com>
2026-01-03 03:50:47 +0300

    task 31 added

--- promises.js
+++ promises.js
@@ -3,21 +3,21 @@
 // ▷ Реализуйте функцию, которая выполняет несколько асинхронных операций последовательно,
 // используя цепочку промисов
 
-function asyncOp1() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("Result 1"), 1000);
-  });
-}
-function asyncOp2() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("Result 2"), 2000);
-  });
-}
-function asyncOp3() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve("Result 3"), 3000);
-  });
-}
+// function asyncOp1() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("Result 1"), 1000);
+//   });
+// }
+// function asyncOp2() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("Result 2"), 2000);
+//   });
+// }
+// function asyncOp3() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve("Result 3"), 3000);
+//   });
+// }
 // Напишите функцию, которая выполняет asyncOp1, затем asyncOp2, затем asyncOp3,
 // и возвращает массив результатов [result1, result2, result3].
 
@@ -98,4 +98,63 @@ function parallelFunc(){
     return Promise.all([asyncOpA(), asyncOpB(), asyncOpC()])
 }
 
-parallelFunc().then(res => console.log(res))
+// parallelFunc().then(res => console.log(res))
+
+
+
+
+
+//===============================     task 31        =========================
+
+
+// ▷ Реализуйте функцию, которая выполняет несколько асинхронных операций последовательно, 
+// но при возникновении ошибки на любой стадии, останавливает выполнение и возвращает ошибку
+
+
+function asyncOp1() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('A'), 1000);
+    });
+}
+function asyncOp2() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('B'), 2000);
+    });
+}
+function asyncOp3() {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve('C'), 3000);
+    });
+}
+function asyncOpWithError() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => reject(new Error('Something went wrong')), 1000);
+    });
+}
+// Напишите функцию, которая выполняет asyncOp1, затем asyncOpWithError, затем asyncOp3,
+// и возвращает результат или ошибку, если что-то пошло не так.
+
+
+async function funcAsyncPar() {
+    try {
+        const res1 = await asyncOp1()
+        console.log(res1)
+
+        const res2 = await asyncOp2()
+        console.log(res2)
+
+        const res3 = await asyncOp3()
+        console.log(res3)
+
+        const error1 = await asyncOpWithError()
+        console.log(error1)
+
+        return [res1, res2, res3, error1]
+        
+    } catch (error) {
+        console.log("Error:", error.message)
+        return error
+    }
+}
+
+// funcAsyncPar()
